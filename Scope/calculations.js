c71705e1b37f14950841ded80ed2f5dfd4bbac6d
wetocean <ikazanci@hotmail.com>
2024-02-26 11:47:28 -0700

    Add count functionality and reset option

--- Scope/calculations.js
+++ Scope/calculations.js
@@ -8,3 +8,35 @@ function calculateTotal() {
    document.getElementById('total').innerText = `The total is: ${Total}`;
 
    }
+
+let count = 0;
+
+function displayCount() {
+    document.getElementById('countDisplay').innerText = count;
+    console.log(count);
+  }
+
+  function checkCountValue() {
+    if (count === 10) {
+      alert("Your Instagram post gained 10 followers! Congratulations!");
+    } else if (count === 20) {
+      alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+  }
+
+function increaseCount() {
+    count++; // Increment the count by 1
+    displayCount(); // Display the count
+    checkCountValue(); // Check if the count is 10 or 20
+  }
+
+  function resetCount() {
+    let reset = confirm("Are you sure you want to reset the count?");
+    if (reset) {
+      count = 0; // Reset the count to 0
+      displayCount(); // Display the count
+    }
+    else {
+      alert("Count has not been reset.");
+    }
+  }
